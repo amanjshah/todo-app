9a2b962a84eb0602628377b01e888aa04bd7884c
Aman Shah <aman.j.shah@gmail.com>
2024-01-10 13:32:47 +0000

    add style & allow user input for login component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -10,16 +10,19 @@ export default function TodoApp() {
 }
 
 function LoginComponent() {
+    const [username, setUsername] = useState('user')
+    const [password, setPassword] = useState('dummy')
+
     return (
         <div className='Login'>
             <div className='LoginForm'>
                 <div>
                     <label>Username: </label>
-                    <input type='text' name='username'/>
+                    <input type='text' name='username' value={username} onChange={(event) => setUsername(event.target.value)}/>
                 </div>
                 <div>
                     <label>Password: </label>
-                    <input type='password' name='password'/>
+                    <input type='password' name='password' value={password} onChange={(event) => setPassword(event.target.value)}/>
                 </div>
                 <div>
                     <button type='button' name='login'>Login</button>
