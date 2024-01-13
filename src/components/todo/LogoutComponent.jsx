6ec811ee643d6562ac0c9f2d7f52357e245d0271
Aman Shah <aman.shah@matillion.com>
2024-01-13 16:54:05 +0000

    eliminate redundant logout component code

--- src/components/todo/LogoutComponent.jsx
+++ src/components/todo/LogoutComponent.jsx
@@ -2,10 +2,4 @@ import {useAuth} from "./security/AuthContext";
 
 export default function LogoutComponent() {
     useAuth().logout()
-
-    return (
-      <div className='Logout'>
-        <h1>Logged out</h1>
-      </div>
-    )
 }
