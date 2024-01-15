e9f2720cf555c92da85acee465fd08016faeac07
Aman Shah <aman.j.shah@gmail.com>
2024-01-15 19:03:34 +0000

    map data from backend to a React state

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,10 +1,13 @@
 import {Link, useParams} from "react-router-dom"
 import axios from "axios"
+import {useState} from "react";
 
 export default function WelcomeComponent() {
+  const [message, setMessage] = useState(null)
+
   function callApi() {
     console.log("called")
-    axios.get("http://localhost:8080/hello-world")
+    axios.get("http://localhost:8080/hello-world-bean")
       .then((response) => successfulResponse(response))
       .catch((error) => errorResponse(error))
       .finally(() => console.log("clean up"))
@@ -12,6 +15,7 @@ export default function WelcomeComponent() {
 
   function successfulResponse(response) {
     console.log(response)
+    setMessage(response.data.message)
   }
 
   function errorResponse(error) {
@@ -24,6 +28,9 @@ export default function WelcomeComponent() {
       <Link to='/todos'>Manage your to-do list</Link>
       <div>
         <button className='btn btn-success m-5' onClick={callApi}>Call hello world api</button>
+      </div>
+      <div className='text-info'>
+        {message}
       </div>
     </div>
   )
