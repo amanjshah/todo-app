bef8bbf99b99e3d4f75dd3b3c2a4afc12709b409
Aman Shah <aman.j.shah@gmail.com>
2024-01-19 13:24:32 +0000

    add form in to-do component to update a to-do list item with new description/target date

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -2,10 +2,12 @@ import {useParams} from "react-router-dom";
 import {getTodo} from "./api/TodoApiService";
 import {useEffect, useState} from "react";
 import {useAuth} from "./security/AuthContext";
+import {Field, Form, Formik} from "formik";
 
 export default function TodoComponent() {
   const {id} = useParams()
-  const [description, setDescription] = useState(null)
+  const [description, setDescription] = useState('')
+  const [targetDate, setTargetDate] = useState('')
   const authContext = useAuth()
 
   useEffect(() => getTodoItem(), [id]);
@@ -14,15 +16,33 @@ export default function TodoComponent() {
     getTodo(authContext.username, id)
       .then((response) => {
         setDescription(response.data.description)
+        setTargetDate(response.data.targetDate)
       })
   }
 
   return (
     <div className='container'>
       <h1>Enter to-do item details</h1>
-      <div>
-        description: {description}
-      </div>
+        <Formik initialValues={{description, targetDate}}
+                enableReinitialize={true}
+                onSubmit={(response) => console.log(response)}
+        >
+          {
+            () => (
+              <Form>
+                <fieldset className='form-group'>
+                  <label>Description</label>
+                  <Field type='text' className='form-control' name='description'/>
+                </fieldset>
+                <fieldset className='form-group'>
+                  <label>Target Date</label>
+                  <Field type='date' className='form-control' name='targetDate'/>
+                </fieldset>
+                <button className='btn btn-success m-5' type="submit">Save</button>
+              </Form>
+            )
+          }
+        </Formik>
     </div>
   )
 }
