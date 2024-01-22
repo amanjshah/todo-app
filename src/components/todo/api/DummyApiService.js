405e7d40adb6d4fb55abdd8141233ee7f3f275dc
Aman Shah <aman.j.shah@gmail.com>
2024-01-22 13:33:50 +0000

    hard-code basic auth token in dummy api service for now

--- src/components/todo/api/DummyApiService.js
+++ src/components/todo/api/DummyApiService.js
@@ -1,3 +1,8 @@
 import {api} from "./ApiClient";
 
-export const dataFromDummyEndpoint = (username) => api.get(`/hello-world/path-variable/${username}`);
+export const dataFromDummyEndpoint =
+  (username) => api.get(`/hello-world/path-variable/${username}`, {
+    headers: {
+      Authorization: 'Basic YW1hbjpkdW1teQ=='
+    }
+  });
