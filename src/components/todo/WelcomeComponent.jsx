63b3d15790d8d23eb0aa642a09a7adf009df387e
Aman Shah <aman.j.shah@gmail.com>
2024-01-28 20:59:53 +0000

    Delete hello world stuff

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,40 +1,12 @@
 import {Link, useParams} from "react-router-dom"
-import {useState} from "react";
-import {dataFromDummyEndpoint} from "./api/DummyApiService";
-import {useAuth} from "./security/AuthContext";
 
 export default function WelcomeComponent() {
 
-  const [message, setMessage] = useState(null)
-  const authContext = useAuth()
-
-  function callApi() {
-    console.log("called")
-    dataFromDummyEndpoint("aman", authContext.token)
-      .then((response) => successfulResponse(response))
-      .catch((error) => errorResponse(error))
-      .finally(() => console.log("clean up"))
-  }
-
-  function successfulResponse(response) {
-    console.log(response)
-    setMessage(response.data.message)
-  }
-
-  function errorResponse(error) {
-    console.log(error)
-  }
 
   return (
     <div className='Welcome'>
       <h1>Welcome {useParams().username}</h1>
       <Link to='/todos'>Manage your to-do list</Link>
-      <div>
-        <button className='btn btn-success m-5' onClick={callApi}>Call hello world api</button>
-      </div>
-      <div className='text-info'>
-        {message}
-      </div>
     </div>
   )
 }
