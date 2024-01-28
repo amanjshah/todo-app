9852711d95bfff0b53876d134c12ae8639019eda
Aman Shah <aman.j.shah@gmail.com>
2024-01-28 21:18:12 +0000

    show capitalised username on landing page for now

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -2,10 +2,15 @@ import {Link, useParams} from "react-router-dom"
 
 export default function WelcomeComponent() {
 
+  function capitalize(username) {
+    return username.charAt(0).toUpperCase() + username.slice(1);
+  }
+
+  const username = useParams().username
 
   return (
     <div className='Welcome'>
-      <h1>Welcome {useParams().username}</h1>
+      <h1>Welcome {capitalize(username)}</h1>
       <button className='btn btn-dark m-5'>
         <Link to='/todos'>Manage your to-do list</Link>
       </button>
