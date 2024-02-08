039f01760df46bcbc4cbd251b64ffd93d598fc9d
Aman Shah <aman.j.shah@gmail.com>
2024-02-08 09:51:27 +0000

    finish display of registration component

--- src/components/todo/RegistrationComponent.jsx
+++ src/components/todo/RegistrationComponent.jsx
@@ -1,3 +1,28 @@
+import {useState} from "react";
+
 export default function RegistrationComponent() {
-  //this is empty
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <div className='Registation'>
+      <h1>Register</h1>
+      <div className='RegistrationForm'>
+        <div>
+          <label>Username: </label>
+          <input type='text' name='username' value={username}
+                 onChange={(event) => setUsername(event.target.value)}/>
+        </div>
+        <div>
+          <label>Password: </label>
+          <input type='password' name='password' value={password}
+                 onChange={(event) => setPassword(event.target.value)}/>
+        </div>
+        <div>
+          <button type='button' name='register'>Register</button>
+        </div>
+      </div>
+    </div>
+  )
 }
